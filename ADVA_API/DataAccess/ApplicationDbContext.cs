8437607c4b3147a533255d1c466d2093fb6c6301
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 14:17:45 +0200

    settup db environment and add first migration

---
+++ ADVA_API/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ADVA_API.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
