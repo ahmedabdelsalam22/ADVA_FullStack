100bfb9559ea90768221a0878c2946b36fa93f9f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 16:10:06 +0200

    create second endPoint to GetEmployeeById

--- ADVA_API/Models/APIResponse.cs
+++ ADVA_API/Models/APIResponse.cs
@@ -4,9 +4,9 @@ namespace ADVA_API.Models
 {
     public class APIResponse
     {
+        public bool IsSuccess { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public List<string>? ErrorMessages { get; set; } = null;
-        public bool IsSuccess { get; set; }
         public Object Result { get; set; }
     }
 }
