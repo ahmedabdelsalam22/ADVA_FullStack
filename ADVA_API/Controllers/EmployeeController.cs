05f3f02a341c53ce4345d642932d308a5bc1a07f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-03 01:53:09 +0200

    some refactoring in endPoint

--- ADVA_API/Controllers/EmployeeController.cs
+++ ADVA_API/Controllers/EmployeeController.cs
@@ -162,6 +162,7 @@ namespace ADVA_API.Controllers
         [HttpDelete("employee/delete/{employeeId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> DeleteEmployee(int? employeeId) 
         {
             try
