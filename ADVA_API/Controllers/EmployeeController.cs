98fe503b8a29f166b2f519e0dd42f4e8ce93c3a9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 18:25:10 +0200

    create third endPoint to CreateEmployee

--- ADVA_API/Controllers/EmployeeController.cs
+++ ADVA_API/Controllers/EmployeeController.cs
@@ -14,7 +14,6 @@ namespace ADVA_API.Controllers
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly APIResponse _ApiResposne;
-
         public EmployeeController(IUnitOfWork unitOfWork,IMapper mapper)
         {
             _unitOfWork = unitOfWork;
@@ -36,7 +35,6 @@ namespace ADVA_API.Controllers
                 {
                     _ApiResposne.IsSuccess = true;
                     _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.Result = new List<Employee>() { };
                     return _ApiResposne;
                 }
 
@@ -65,8 +63,7 @@ namespace ADVA_API.Controllers
                 {
                     _ApiResposne.IsSuccess = false;
                     _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.Result = null;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employeeId must't be 0 or null" };
+                    _ApiResposne.ErrorMessages = new List<string>() { "employee Id must't be 0 or null" };
                     return _ApiResposne;
                 }
                 Employee employee = await _unitOfWork.employeeRepository.Get(filter:x=>x.Id == employeeId ,tracked: false,
@@ -75,7 +72,6 @@ namespace ADVA_API.Controllers
                 {
                     _ApiResposne.IsSuccess = true;
                     _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.Result = null;
                     return _ApiResposne;
                 }
 
@@ -91,6 +87,52 @@ namespace ADVA_API.Controllers
                 _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
                 return _ApiResposne;
             }
+        }
+        [HttpPost("employee/create")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> CreateEmployee([FromBody] Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    _ApiResposne.IsSuccess = false;
+                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
+                    _ApiResposne.ErrorMessages = new List<string>() { "employee must't null" };
+                    return _ApiResposne;
+                }
+
+                // we must sure that related entities is exists in database 
+                Department department = await _unitOfWork.departmentRepository.Get(filter: x => x.Name.ToLower() == employee.Department.Name.ToLower());
+                Employee manager = await _unitOfWork.employeeRepository.Get(filter: x => x.Name.ToLower() == employee.Manager.Name.ToLower());
+
+                if (department == null && manager == null)
+                {
+                    _ApiResposne.IsSuccess = false;
+                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
+                    _ApiResposne.ErrorMessages = new List<string>() { "department or manager does't exists" };
+                    return _ApiResposne;
+                }
+
+                employee.Department = department!;
+                employee.Manager = manager;
+
+                await _unitOfWork.employeeRepository.Create(employee);
+
+                _ApiResposne.IsSuccess = true;
+                _ApiResposne.StatusCode = HttpStatusCode.OK;
+                return _ApiResposne;
+            }
+            catch (Exception ex) 
+            {
+                _ApiResposne.IsSuccess = false;
+                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
+                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
+                return _ApiResposne;
+            }
+
         }
     }
 }
