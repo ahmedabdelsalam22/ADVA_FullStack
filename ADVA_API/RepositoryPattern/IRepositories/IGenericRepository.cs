52da86540a6a47ff96bf8a4e71859112d9fcb215
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 15:11:20 +0200

    create IGenericRepository and its implementation in GenericRepository

---
+++ ADVA_API/RepositoryPattern/IRepositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace ADVA_API.RepositoryPattern.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null, bool tracked = true, String? includeProperties = null);
+        Task<T> Get(Expression<Func<T, bool>>? filter = null, bool tracked = true, String? includeProperties = null);
+        Task Create(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ ADVA_API/RepositoryPattern/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using ADVA_API.DataAccess;
+using ADVA_API.RepositoryPattern.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace ADVA_API.RepositoryPattern.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+
+        }
+        public Task<T> Get(Expression<Func<T, bool>>? filter = null, bool tracked = true, String? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.FirstOrDefaultAsync();
+        }
+        public Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null, bool tracked = true, String? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return query.ToListAsync();
+        }
+    }
+}
