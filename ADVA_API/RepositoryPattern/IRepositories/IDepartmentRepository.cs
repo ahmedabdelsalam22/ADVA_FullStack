cfc73776ce293b89dacc0de6107d252228c3d7c7
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 15:18:23 +0200

    create IUnitOfWork to deal as interface combile set of interfaces

---
+++ ADVA_API/RepositoryPattern/IRepositories/IDepartmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using ADVA_API.Models;
+
+namespace ADVA_API.RepositoryPattern.IRepositories
+{
+    public interface IDepartmentRepository : IGenericRepository<Department>
+    {
+        Task Update(Department department);
+    }
+}
---
+++ ADVA_API/RepositoryPattern/Repositories/DepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using ADVA_API.DataAccess;
+using ADVA_API.Models;
+using ADVA_API.RepositoryPattern.IRepositories;
+
+namespace ADVA_API.RepositoryPattern.Repositories
+{
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public DepartmentRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public async Task Update(Department department)
+        {
+            _context.Update(department);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
