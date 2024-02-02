2d13db6d4153a757c8a8d44167742a11fe4532dd
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-03 01:13:52 +0200

    fix issue in EmployeeDto

--- ADVA_API/Models/DTOS/EmployeeDto.cs
+++ ADVA_API/Models/DTOS/EmployeeDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ADVA_API.Models.DTOS
 {
@@ -8,8 +9,10 @@ namespace ADVA_API.Models.DTOS
         public string Name { get; set; }
         public decimal Salary { get; set; }
         public int DepartmentID { get; set; }
+        [JsonIgnore]
         public virtual Department Department { get; set; }
         public int? ManagerID { get; set; }
+        [JsonIgnore]
         public virtual Employee Manager { get; set; }
     }
 }
