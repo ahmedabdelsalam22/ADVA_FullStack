e307db05977288bdec18621d33fcd8aeba3ea281
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-03 01:49:55 +0200

    refactor in APIResponse format in EmployeeController

--- ADVA_API/Controllers/EmployeeController.cs
+++ ADVA_API/Controllers/EmployeeController.cs
@@ -14,19 +14,17 @@ namespace ADVA_API.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        private readonly APIResponse _ApiResposne;
         public EmployeeController(IUnitOfWork unitOfWork,IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _ApiResposne = new APIResponse();
         }
 
         [HttpGet("employees")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<APIResponse>> GetEmployees()
+        public async Task<ActionResult> GetEmployees()
         {
             try
             {
@@ -34,79 +32,57 @@ namespace ADVA_API.Controllers
                                includes: new string[] { "Department", "Manager" });
                 if (employees == null)
                 {
-                    _ApiResposne.IsSuccess = true;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    return _ApiResposne;
+                    return NotFound();
                 }
 
                 List<EmployeeDto> employeeDtos = _mapper.Map<List<EmployeeDto>>(employees);
 
-                _ApiResposne.IsSuccess = true;
-                _ApiResposne.StatusCode = HttpStatusCode.OK;
-                _ApiResposne.Result = employeeDtos;
-                return _ApiResposne;
+                return Ok(employeeDtos);
             }
             catch (Exception ex) 
             {
-                _ApiResposne.IsSuccess = false;
-                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
-                return _ApiResposne;
+                return BadRequest(ex.Message.ToString());
             }
         }
         [HttpGet("employee/{employeeId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<APIResponse>> GetEmployeeById(int? employeeId) 
+        public async Task<ActionResult> GetEmployeeById(int? employeeId) 
         {
             try
             {
                 if (employeeId == 0 || employeeId == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee Id must't be 0 or null" };
-                    return _ApiResposne;
+                    return BadRequest("employee Id must't be 0 or null");
                 }
                 Employee employee = await _unitOfWork.employeeRepository.Get(filter:x=>x.Id == employeeId ,tracked: false,
                                    includes: new string[] { "Department", "Manager" });
                 if (employee == null)
                 {
-                    _ApiResposne.IsSuccess = true;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    return _ApiResposne;
+                    return NotFound();
                 }
 
                 EmployeeDto employeeDto = _mapper.Map<EmployeeDto>(employee);
 
-                _ApiResposne.IsSuccess = true;
-                _ApiResposne.StatusCode = HttpStatusCode.OK;
-                _ApiResposne.Result = employeeDto;
-                return _ApiResposne;
+                return Ok();
             }
             catch (Exception ex)
             {
-                _ApiResposne.IsSuccess = false;
-                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
-                return _ApiResposne;
+                return BadRequest(ex.Message.ToString());
             }
         }
         [HttpPost("employee/create")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<APIResponse>> CreateEmployee([FromBody] EmployeeCreateDto employeeCreateDto)
+        public async Task<ActionResult> CreateEmployee([FromBody] EmployeeCreateDto employeeCreateDto)
         {
             try
             {
                 if (employeeCreateDto == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee must't null" };
-                    return _ApiResposne;
+                    return BadRequest("employee must't null");
                 }
 
                 // we must sure that related entities is exists in database 
@@ -116,10 +92,7 @@ namespace ADVA_API.Controllers
 
                 if (department == null && manager == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                    _ApiResposne.ErrorMessages = new List<string>() { "department or manager does't exists" };
-                    return _ApiResposne;
+                    return NotFound();
                 }
 
                 employeeCreateDto.Department = department!;
@@ -129,16 +102,11 @@ namespace ADVA_API.Controllers
 
                 await _unitOfWork.employeeRepository.Create(employeeToDb);
 
-                _ApiResposne.IsSuccess = true;
-                _ApiResposne.StatusCode = HttpStatusCode.OK;
-                return _ApiResposne;
+                return Created();
             }
             catch (Exception ex) 
             {
-                _ApiResposne.IsSuccess = false;
-                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
-                return _ApiResposne;
+                return BadRequest(ex.Message.ToString());
             }
 
         }
@@ -146,32 +114,23 @@ namespace ADVA_API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<APIResponse>> UpdateEmployee([FromBody] EmployeeUpdateDto employeeDto , int employeeId)
+        public async Task<ActionResult> UpdateEmployee([FromBody] EmployeeUpdateDto employeeDto , int employeeId)
         {
             try
             {
                 
                 if (employeeDto == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee must't be null" };
-                    return _ApiResposne;
+                    return BadRequest("employee must't be null");
                 }
                 if (employeeId != employeeDto.Id)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee Id must be tha same of employee which need to Update" };
-                    return _ApiResposne;
+                    return BadRequest("employee Id must be tha same of employee which need to Update");
                 }
                 Employee employeeFromDb = await _unitOfWork.employeeRepository.Get(filter: x => x.Id == employeeId, tracked: false);
                 if (employeeFromDb == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee does't exists" };
-                    return _ApiResposne;
+                    return NotFound("no employee found with this id");
                 }
 
                 // we must sure that related entities is exists in database 
@@ -181,10 +140,7 @@ namespace ADVA_API.Controllers
 
                 if (department == null && manager == null)
                 {
-                    _ApiResposne.IsSuccess = false;
-                    _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                    _ApiResposne.ErrorMessages = new List<string>() { "department or manager does't exists" };
-                    return _ApiResposne;
+                    return NotFound("department or manager does't exists");
                 }
 
                 employeeDto.Department = department!;
@@ -195,51 +151,36 @@ namespace ADVA_API.Controllers
 
                 await _unitOfWork.employeeRepository.Update(employeeToDb);
 
-                _ApiResposne.IsSuccess = true;
-                _ApiResposne.StatusCode = HttpStatusCode.OK;
-                return _ApiResposne;
+                return Ok("employee updated successfully");
             }
             catch (Exception ex)
             {
-                _ApiResposne.IsSuccess = false;
-                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
-                return _ApiResposne;
+                return BadRequest(ex.Message.ToString());
             }
 
         }
         [HttpDelete("employee/delete/{employeeId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<APIResponse>> DeleteEmployee(int? employeeId) 
+        public async Task<ActionResult> DeleteEmployee(int? employeeId) 
         {
             try
             {
                 if (employeeId == 0 || employeeId == null)
                 {
-                    _ApiResposne.IsSuccess = true;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    _ApiResposne.ErrorMessages = new List<string>() { "employee Id must't be 0 or null" };
-                    return _ApiResposne;
+                    return BadRequest("employee Id must't be 0 or null");
                 }
                 Employee employeeToDelete = await _unitOfWork.employeeRepository.Get(filter: x => x.Id == employeeId, tracked: false);
                 if (employeeToDelete == null)
                 {
-                    _ApiResposne.IsSuccess = true;
-                    _ApiResposne.StatusCode = HttpStatusCode.NotFound;
-                    return _ApiResposne;
+                    return NotFound("no employee found with this id");
                 }
                 await _unitOfWork.employeeRepository.Delete(employeeToDelete);
-                _ApiResposne.IsSuccess = true;
-                _ApiResposne.StatusCode = HttpStatusCode.OK;
-                return _ApiResposne;
+                return Ok("employee deleted successfully");
             }
             catch (Exception ex)
             {
-                _ApiResposne.IsSuccess = false;
-                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
-                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
-                return _ApiResposne;
+                return BadRequest(ex.Message.ToString());
             }
         }
     }
