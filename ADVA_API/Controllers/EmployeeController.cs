02ed8e9b06d0c90be147e88fdabe1785f979261d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 15:56:21 +0200

    create first endPoint to getl all employees

--- ADVA_API/Controllers/EmployeeController.cs
+++ ADVA_API/Controllers/EmployeeController.cs
@@ -3,10 +3,11 @@ using ADVA_API.RepositoryPattern.Unit_Of_Work;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ADVA_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class EmployeeController : ControllerBase
     {
@@ -20,5 +21,37 @@ namespace ADVA_API.Controllers
             _mapper = mapper;
             _ApiResposne = new APIResponse();
         }
+
+        [HttpGet("employees")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetEmployees()
+        {
+            try
+            {
+                List<Employee> employees = await _unitOfWork.employeeRepository.GetAll(tracked: false,
+                               includes: new string[] { "Department", "Manager" });
+                if (employees == null)
+                {
+                    _ApiResposne.IsSuccess = true;
+                    _ApiResposne.StatusCode = HttpStatusCode.OK;
+                    _ApiResposne.Result = new List<Employee>() { };
+                    return _ApiResposne;
+                }
+
+                _ApiResposne.IsSuccess = true;
+                _ApiResposne.StatusCode = HttpStatusCode.OK;
+                _ApiResposne.Result = employees;
+                return _ApiResposne;
+            }
+            catch (Exception ex) 
+            {
+                _ApiResposne.IsSuccess = false;
+                _ApiResposne.StatusCode = HttpStatusCode.BadRequest;
+                _ApiResposne.ErrorMessages = new List<string>() { ex.ToString() };
+                return _ApiResposne;
+            }
+        }
+
     }
 }
