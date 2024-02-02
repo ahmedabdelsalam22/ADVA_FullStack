439f057f1abc865d3a86e71757279ab142fbde0d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2024-02-02 15:14:11 +0200

    create IEmployeeRepository and its implementation

---
+++ ADVA_API/RepositoryPattern/IRepositories/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using ADVA_API.Models;
+
+namespace ADVA_API.RepositoryPattern.IRepositories
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        Task Update(Employee employee);
+    }
+}
---
+++ ADVA_API/RepositoryPattern/Repositories/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using ADVA_API.DataAccess;
+using ADVA_API.Models;
+using ADVA_API.RepositoryPattern.IRepositories;
+
+namespace ADVA_API.RepositoryPattern.Repositories
+{
+    public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task Update(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
